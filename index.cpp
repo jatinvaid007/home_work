c0fbadabb4e19911578c0d6375eded5b0b796ddd
jatinvaid108 <156705942+jatinvaid108@users.noreply.github.com>
2024-03-08 15:36:33 +0530

    Hello world program added

--- index.cpp
+++ index.cpp
@@ -1,7 +1,13 @@
+// #include <iostream>
+// using namespace std;
+// int num;
+// int main(){
+//   if (num%2==0) cout<<"Even number";
+//   else cout<<"Odd number";
+// }
+
 #include <iostream>
 using namespace std;
-int num;
 int main(){
-  if (num%2==0) cout<<"Even number";
-  else cout<<"Odd number";
+  cout<<"Hello World";
 }
